d132ad4252a9f562bc8ce2e18a6a3fb9ddcf518a
rtesei <romeo.tesei@etu.u-bordeaux.fr>
2022-03-15 14:45:34 +0100

    mmmmh

--- scrap.js
+++ scrap.js
@@ -9,22 +9,21 @@ const puppeteer = require('puppeteer');
 
     await page.setViewport({ width: 1200, height: 720 });
 
-    await page.goto('https://www.websudoku.com/');
+    await page.goto('https://soduko-online.com/');
     sleep(1000)
 
-    let grid = await page.$$('#puzzle_grid > tbody > tr')
+    let grid = await page.$$("#y6 > div")
 
-    let myBoard = []
+    let cases = []
 
-    console.log(grid);
-
-    // grid[0].forEach(row => {
-    //     console.log(row);
-    //     myBoard.push(row)
-    // })
-
-    console.log(myBoard);
-
+    grid.forEach(carre => {
+        currentZone = []
+        for (let caze of carre) {
+            currentZone.push(page.evaluate(el => el.textContent, caze[0]))
+        }
+        cases.push(currentZone)
+    })
+    console.log(cases);
 
 
 
